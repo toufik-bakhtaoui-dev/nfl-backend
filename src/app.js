5f1f2b3124db05b8f6e3d3c87e312c5c817732a7
toufik-bakhtaoui-dev <toufik@MacBook-Pro-de-toufik.local>
2020-02-19 20:27:29 +0100

    test

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const database = require('../config/db.config')
 const fastify = require('fastify')()
 
+//database()
+
 fastify.get('/', (req, reply) => {
     reply.send('Welcome to the nfl!')
 })
-
-database()
 
 module.exports = fastify
